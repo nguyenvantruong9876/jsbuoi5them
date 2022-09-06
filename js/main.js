66d4e24606b765fdfec0b70c5361b518904ce095
nguyenvantruong9876 <nguyenvantruong9876>
2022-09-06 09:14:18 +0700

    xoa clg

--- js/main.js
+++ js/main.js
@@ -16,26 +16,26 @@ function thuecanhan() {
     var thuephaichiu = thunhapnam - 4e+6 - 16e+5 * phuthuoc;
     var thue;
     if (thuephaichiu > 0 && thuephaichiu <= 6e7) {
-        console.log("Giá 60 Triệu")
+        
         thue = Den_60 * thuephaichiu;
     } else if (thuephaichiu > 6e7 && thuephaichiu <= 12e7) {
-        console.log("Giá 60 Đến 120")
+     
         thue = Den_60den_120 * thuephaichiu;
     } else if (thuephaichiu > 12e7 && thuephaichiu <= 21e7) {
-        console.log("Giá 120 Đến 210")
+   
         thue = den_120den_210 * thuephaichiu;
     } else if (thuephaichiu < 21e7 && thuephaichiu <= 384e6) {
-        console.log("Giá 210 Đến 384")
+   
         thue = den_210den_384 * thuephaichiu;
     } else if (thuephaichiu < 384e6 && thuephaichiu <= 624e6) {
-        console.log("Giá 384 Đến 624")
+ 
         thue = den_384den_624 * thuephaichiu;
     } else if (624e6 < thuephaichiu && thuephaichiu <= 960e6) {
-        console.log("Giá 624 Đến 960")
+
         thue = den_624den_960 * thuephaichiu;
     }
     else if (thuephaichiu > 960e6) {
-        console.log("Giá Thuế Còn lại")
+    
         thue = tren960 * thuephaichiu;
     } else {
         alert("Số tiền thu nhập không hợp lệ")
